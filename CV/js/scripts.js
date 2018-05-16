7f69faf0cbb8a7289cb928b2a6bbfe8989e7ab4a
fabrice_duchenois <fabrice.duchenois@gmail.com>
2018-05-16 09:51:47 +0200

    add js

---
+++ CV/js/scripts.js
@@ -0,0 +1,8 @@
+function showDetails(){
+  var x = document.getElementById("address");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
