0cbdc8c4459f9a9672b8935833a2d2d5d64b5631
fabrice_duchenois <fabrice.duchenois@gmail.com>
2018-05-17 09:57:58 +0200

    modif click

--- CV/js/scripts.js
+++ CV/js/scripts.js
@@ -2,7 +2,8 @@ function showDetails(){
   var x = document.getElementById("address");
     if (x.style.display == "none") {
         x.style.display = "block";
-    } else {
+    }
+    else {
         x.style.display = "none";
     }
 }
