535ab223ff485af30b1772b007d7220c1ff3ef72
Simplon Charleville <38696381+SimplonCharleville@users.noreply.github.com>
2018-05-17 11:06:12 +0200

    Update scripts.js

--- CV/js/scripts.js
+++ CV/js/scripts.js
@@ -1,9 +1,17 @@
+// On écris sa fonction javascript
 function showDetails(){
+  // On défini la variable "x" qui va cibler notre élément HTML avec l'ID "address"
+  // C'est pour simplifier son code
   var x = document.getElementById("address");
-    if (x.style.display == "none") {
-        x.style.display = "block";
-    }
-    else {
-        x.style.display = "none";
-    }
+  
+  // On écris la condition qui va vérifier le style de notre balise HTML
+  if (x.style.display == "none") {
+      // Si la condition est remplie (l'élément est masqué), alors on l'affiche
+      x.style.display = "block";
+   }
+   // Si la condition n'est pas remplie (l'élément est affiché), alors on le masque
+   else
+   {
+      x.style.display = "none";
+   }
 }
